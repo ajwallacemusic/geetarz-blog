2677eeb90c1eba7f4a69de9ee9b0c5a63e52f401
AJ Wallace <alexander.j.wallace@centene.com>
2019-05-31 16:50:12 -0500

    start customizing

---
+++ src/pages/new-page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default () => (
+  <div className="section">
+  <div className="container">
+    <div className="columns has-text-centered">
+      <div className="column is-5 is-offset-5">
+        Hello World
+      </div>
+    </div>
+  </div>
+  </div>
+)
