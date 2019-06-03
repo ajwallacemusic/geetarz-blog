d1d19a20a7bb55a5bd63aba2d76ce4c4af331b88
AJ Wallace <alexander.j.wallace@centene.com>
2019-06-03 14:49:37 -0500

    adjust some fonts

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -34,7 +34,7 @@ export const IndexPageTemplate = ({
         }}
       >
         <h1
-          className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
+          className="is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
           style={{
             backgroundColor: imagetext.text ? imagetext.backgroundcolor : 'white',
             color: 'white',
@@ -66,8 +66,8 @@ export const IndexPageTemplate = ({
                   </div>
                 </div>
                 <div className="column is-12">
-                  <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
+                  <h3 className="has-text-weight-normal is-size-2">
+                    Latest Stories
                   </h3>
                   <BlogRoll />
                   <div className="column is-12 has-text-centered">
