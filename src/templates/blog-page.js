52287c2b4f8afe86f81b7a6a66897457203eba2b
AJ Wallace <alexander.j.wallace@centene.com>
2019-06-18 12:11:58 -0500

    blog template

--- src/templates/blog-page.js
+++ src/templates/blog-page.js
@@ -2,11 +2,15 @@ import React from 'react'
 
 import Layout from '../components/Layout'
 import BlogRoll from '../components/BlogRoll'
+import { graphql } from 'gatsby'
+import PropTypes from 'prop-types'
 
-export default class BlogPageTemplate extends React.Component {
-  render() {
-    return (
-      <Layout>
+export const BlogPageTemplate = ({
+  image,
+  imagetext,
+  main,
+}) => (
+      <div>
         <div
           className="full-width-image-container margin-top-0"
           style={{
@@ -22,7 +26,7 @@ export default class BlogPageTemplate extends React.Component {
               padding: '1rem',
             }}
           >
-            Latest Stories
+            {imagetext}
           </h1>
         </div>
         <section className="section">
@@ -32,7 +36,79 @@ export default class BlogPageTemplate extends React.Component {
             </div>
           </div>
         </section>
-      </Layout>
+        </div>
     )
-  }
+BlogPageTemplate.propTypes = {
+      image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+      imagetext: PropTypes.object,
+      main: PropTypes.object,
+      excerpt: PropTypes.string,
+    }
+    
+    const BlogPage = ({ data }) => {
+      const { frontmatter } = data.markdownRemark
+    
+      return (
+        <Layout>
+          <BlogPageTemplate
+            image={frontmatter.image}
+            imagetext={frontmatter.imagetext}
+            main={frontmatter.main}
+          />
+        </Layout>
+      )
+    }
+
+
+BlogPage.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object,
+    })
+  })
 }
+
+export default BlogPageTemplate
+
+export const blogPageQuery = graphql`
+  query BlogPageTemplate {
+    markdownRemark(frontmatter: {templateKey: {eq: "blog-page"}}) {
+      frontmatter {
+        imagetext {
+          text
+          backgroundcolor
+        }
+        image {
+          childImageSharp {
+            fluid(maxWidth: 2048, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        main {
+          heading
+          text
+        }
+      }
+    }
+    markdownRemark(frontmatter: {templateKey: {eq: "blog-page"}}) {
+      frontmatter {
+        imagetext {
+          text
+          backgroundcolor
+        }
+        image {
+          childImageSharp {
+            fluid(maxWidth: 2048, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        main {
+          heading
+          text
+        }
+      }
+    }
+  }
+`
